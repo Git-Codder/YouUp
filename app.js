c5553a90483d8e0478279937c0544efe3ad5a5bf
just_easy <56085648+Git-Codder@users.noreply.github.com>
2020-07-12 22:44:02 +0530

    update app.js
    change env port number

--- app.js
+++ app.js
@@ -103,6 +103,6 @@ app.use(userRouts);
 app.use(otherRouts);
 
 //setting env for running application
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000,process.env.IP, function () {
     console.log("App listening on port 3000!");
 });
