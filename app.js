c85b5cca70b830f6987d4e1e36a649661327ba79
Aditya Kumawat <56085648+Git-Codder@users.noreply.github.com>
2021-01-25 14:55:35 +0530

    update app.js
    change commented code

--- app.js
+++ app.js
@@ -131,6 +131,6 @@ app.use(userRouts);
 app.use(otherRouts);
 
 //setting env for running application
-// app.listen(process.env.PORT ,process.env.IP, function () {
-//     console.log("App listening on port 3000!");
-// });
+app.listen(process.env.PORT ,process.env.IP, function () {
+    console.log("App listening on port 3000!");
+});
