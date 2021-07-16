a4f93d74b72f6b33d8d01d718fff25777bd689b2
Aditya <kumawataditya1358@gmail.com>
2021-07-16 14:08:46 +0530

    fix  UI bug and also fix data missing bug

--- app.js
+++ app.js
@@ -131,6 +131,9 @@ app.use(userRouts);
 app.use(otherRouts);
 
 //setting env for running application
-app.listen(process.env.PORT ,process.env.IP, function () {
+// app.listen(process.env.PORT ,process.env.IP, function () {
+//     console.log("App listening on port 3000!");
+// });
+app.listen(3000, function () {
     console.log("App listening on port 3000!");
 });
